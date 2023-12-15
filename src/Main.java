18dbd02ec00cfc7d47062e68370dec283f42be89
jennifer j <jenniferjuarezjj@gmail.com>
2023-12-15 15:51:10 -0500

    move main function

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,4 @@
 public class Main {
-    public static void main(String[] args) {
-        //test case 1
-        int [] arr1 = {1, 2, 3, 4, 5};
-        int n1 = 5; // size of the array
-        int k1 = 2; //number of iterations
-        rotateArray(n1, k1, arr1);  //calling rotateArray method
-        printArray("Test Case 1: ", arr1);  //calling printArray method
-
-
-        int [] arr2 = {4, 5, 6, 7, 8};
-        int n2 = 5;
-        int k2 = 4;
-        rotateArray(n2, k2, arr2);
-        printArray("Test Case 2: ", arr2);
-
-    }
 
     public static void rotateArray(int n, int k, int [] arr) {
         //the outer loop runs k times, representing the number of rotations
@@ -40,6 +24,21 @@ public class Main {
         System.out.println();
 
     }
+    public static void main(String[] args) {
+        //test case 1
+        int [] arr1 = {1, 2, 3, 4, 5};
+        int n1 = 5; // size of the array
+        int k1 = 2; //number of iterations
+        rotateArray(n1, k1, arr1);  //calling rotateArray method
+        printArray("Test Case 1: ", arr1);  //calling printArray method
 
+
+        int [] arr2 = {4, 5, 6, 7, 8};
+        int n2 = 5;
+        int k2 = 4;
+        rotateArray(n2, k2, arr2);
+        printArray("Test Case 2: ", arr2);
+
+    }
 
 }
